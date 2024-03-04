7ab94f6228994e0001db1e2c3c0ad9e51ee22637
Vladimir Fedorenko <140750008+FedorenkoVladimirM@users.noreply.github.com>
2024-03-04 08:43:30 +0300

    commit 3

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-let str = "js";
+let str = "js"; //Задание 1
 str = str.toUpperCase();
 console.log(str);
 
-function filterStrings(array, startStr) {
+function filterStrings(array, startStr) { //Задание 2
     return array.filter(item => item.toLowerCase().startsWith(startStr.toLowerCase()));
 }
 
-let number = 32.58884;
+let number = 32.58884; //Задание 3
 console.log(Math.floor(number));
 console.log(Math.ceil(number));
 console.log(Math.round(number));
 
-let nums = [52, 53, 49, 77, 21, 32];
+let nums = [52, 53, 49, 77, 21, 32]; //Задание 4
 console.log(Math.min(...nums));
 console.log(Math.max(...nums));
 
-function randomOneToTen(){
+function randomOneToTen(){ //Задание 5
     console.log(Math.floor(Math.random() * 10) + 1);
 }
 
-function randomNumbers(n){
+function randomNumbers(n){ //Задание 6
     let result = [];
     for (let i = 0; i < n/2; i++) {
         result.push(Math.floor(Math.random() * n));
@@ -27,16 +27,25 @@ function randomNumbers(n){
     return result;
 }
 
-function randomBetween (min, max) {
+function randomBetween (min, max) { //Задание 7
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-console.log(new Date());
+console.log(new Date()); //Задание 8
 
-let currenDate = new Date();
+let currenDate = new Date(); //Задание 9
 currenDate.setDate(currenDate.getDate() + 73);
 console.log(currenDate);
 
-function formatDate(date) {
-    let days = ["Воскресенье"]
+function formatDate(date) { //Задание 10
+    let days = ["Воскресенье", "Понеделььник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    let months = ["января", "феевраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+    return 
+    `Дата:
+    ${data.getDate()}
+    ${months[date.getMonth()]}
+    ${data.getFullYear()} - это ${days[date.getDay()]}\nВремя:
+    ${date.getHours()}:${date.getMinytes()}:${date.getSeconds()}`; 
 }
+
+console.log(formatDate(new Date()));
